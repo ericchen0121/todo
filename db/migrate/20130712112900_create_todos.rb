def971a670c94ed5aec6c1d3dc2f301ae1052382
dbc08 <apprentice@dbc08.local>
2013-07-12 11:44:01 -0700

    initial commit

---
+++ db/migrate/20130712112900_create_todos.rb
@@ -0,0 +1,13 @@
+require_relative '../config'
+
+# this is where you should use an ActiveRecord migration to 
+
+class CreateTodos < ActiveRecord::Migration
+  def change
+    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :todos do |t|
+      t.string :task
+      t.integer :completed
+    end
+  end
+end
