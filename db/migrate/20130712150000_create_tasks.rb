0f05061bb931395e996bf202e6e88d3bc6768399
dbc08 <apprentice@dbc08.local>
2013-07-12 17:05:00 -0700

    Deleted old migration, cleaned up create_tasks.rb migration

---
+++ db/migrate/20130712150000_create_tasks.rb
@@ -0,0 +1,13 @@
+# this is where you should use an ActiveRecord migration to 
+
+class CreateTasks < ActiveRecord::Migration
+  def change
+    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :tasks do |t|
+      t.string :description
+      t.datetime :completed_at
+
+      t.timestamps
+    end
+  end
+end
