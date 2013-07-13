e2930a6247b91ae1ef9e0c28da4bb099553905f2
dbc08 <apprentice@dbc08.local>
2013-07-12 17:58:47 -0700

    list and delete methods

--- app/controllers/controller.rb
+++ app/controllers/controller.rb
@@ -1,3 +1,10 @@
+controller = TaskController.new
+
+# ruby ... list
+if ARGV[0] == 'list'
+  controller.list
+end
+
 module TaskViewer
 
   def show(todo)
@@ -12,5 +19,16 @@ class TaskController
   def add(description)
     Task.create(description: description)
   end
-  
+
+  def list
+    tasklist = Task.all.to_a # => Array
+    tasklist.each do |task|
+      puts "#{task.id}. #{task.description}"
+    end
+  end
+
+  def delete(id)
+    Task.destroy(id)
+  end
+
 end
